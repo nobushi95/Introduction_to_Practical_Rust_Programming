e73451a25b24e224296d9cb6fb80e756d8ec0eee
nobushi95 <nobushi1121@gmail.com>
2022-08-11 16:58:05 +0900

    ウィジェットの配置

--- ch07/ui_demo/src/main.rs
+++ ch07/ui_demo/src/main.rs
@@ -1,12 +1,17 @@
-use iced::{button, executor, Application, Clipboard, Command, Element, Font, Settings, Text};
+use iced::{
+    button, executor, Align, Application, Button, Clipboard, Column, Command, Element, Font,
+    Length, Row, Settings, Text,
+};
 
 const FONT: Font = Font::External {
     name: "PixelMplus12-Regular",
-    bytes: include_bytes!("../rsc/PixelMplus12-Regular"),
+    bytes: include_bytes!("../rsc/PixelMplus12-Regular.ttf"),
 };
 
 fn main() -> iced::Result {
-    GUI::run(Settings::default())
+    let mut settings = Settings::default();
+    settings.window.size = (400u32, 120u32);
+    GUI::run(settings);
 }
 
 struct GUI {
@@ -46,5 +51,34 @@ impl Application for GUI {
 
     fn view(&mut self) -> Element<'_, Self::Message> {
         let tick_text = Text::new("00:00:00.00").font(FONT).size(60);
+        let start_stop_button = Button::new(
+            &mut self.start_stop_button_state,
+            Text::new("Start")
+                .horizontal_alignment(iced::HorizontalAlignment::Center)
+                .font(FONT),
+        )
+        .min_width(80);
+
+        let reset_button = Button::new(
+            &mut self.reset_button_state,
+            Text::new("Reset")
+                .horizontal_alignment(iced::HorizontalAlignment::Center)
+                .font(FONT),
+        );
+
+        Column::new()
+            .push(tick_text)
+            .push(
+                Row::new()
+                    .push(start_stop_button)
+                    .push(reset_button)
+                    .spacing(10),
+            )
+            .spacing(10)
+            .padding(10)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .align_items(Align::Center)
+            .into()
     }
 }
